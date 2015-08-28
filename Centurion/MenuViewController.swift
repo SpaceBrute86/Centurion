da8c9f72c1cc38aa71c33bdeb9bbd889b7956b86
rmark-0201A07CD <dev.markwick@gmail.com>
2015-08-27 23:57:32 -0400

    Add menu VC

---
+++ Centurion/MenuViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Centurion
+//
+//  Created by Robbie Markwick on 11/27/14.
+//
+//
+
+import UIKit
+
+class MenuViewController: UIViewController {
+
+	class func dismiss(){
+		(UIApplication.sharedApplication().delegate as! AppDelegate).window?.rootViewController?.dismissViewControllerAnimated(true, completion: nil)
+	}
+	override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+		if segue.identifier == "1P" {
+			(segue.destinationViewController as! GameViewController).isSingle = true
+			//IS SINGLE PLAYER
+		}
+	}
+}
